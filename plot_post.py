f0c84e79a64e8eb2aba54364c4bc0d781256cf33
aloctavodia <aloctavodia@gmail.com>
2016-12-26 12:45:07 -0300

    replace HDI by HPD and remove mean_plot

--- plot_post.py
+++ plot_post.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
-from hdi import hdi_grid
+from hpd import hpd_grid
 
 
 def plot_post(param_sample_vec, cred_mass=0.95, comp_val=None,
@@ -18,17 +18,17 @@ def plot_post(param_sample_vec, cred_mass=0.95, comp_val=None,
     0.40784313725490196), (0.7686274509803922, 0.3058823529411765, 
     0.3215686274509804)]
 
-    post_summary = {'mean':0,'median':0,'mode':0, 'hdi_mass':0,'hdi_low':0,
-                   'hdi_high':0, 'comp_val':0, 'pc_gt_comp_val':0, 'ROPE_low':0,
+    post_summary = {'mean':0,'median':0,'mode':0, 'hpd_mass':0,'hpd_low':0,
+                   'hpd_high':0, 'comp_val':0, 'pc_gt_comp_val':0, 'ROPE_low':0,
                    'ROPE_high':0, 'pc_in_ROPE':0}
 
     post_summary['mean'] = round(np.mean(param_sample_vec), roundto)
     post_summary['median'] = round(np.median(param_sample_vec), roundto)
-    post_summary['hdi_mass'] = cred_mass
+    post_summary['hpd_mass'] = cred_mass
 
-    # Compute the HDI, KDE and mode for the posterior
-    hdi, x, y, modes = hdi_grid(param_sample_vec, cred_mass, roundto)
-    post_summary['hdi'] = hdi
+    # Compute the HPD, KDE and mode for the posterior
+    hpd, x, y, modes = hpd_grid(param_sample_vec, cred_mass, roundto)
+    post_summary['hpd'] = hpd
     post_summary['mode'] = modes
 
     ## Plot KDE.
@@ -64,12 +64,12 @@ def plot_post(param_sample_vec, cred_mass=0.95, comp_val=None,
         post_summary['ROPE_high'] = ROPE[1] 
         post_summary['pc_in_ROPE'] = pc_in_ROPE
 
-    ## Display the HDI.
-    hdi_label = ''
-    for value in hdi:
+    ## Display the HPD.
+    hpd_label = ''
+    for value in hpd:
         plt.plot(value, [0, 0], linewidth=8, color='k')
-        hdi_label = hdi_label +  '{:g} {:g}\n'.format(round(value[0], roundto), round(value[1], roundto)) 
-    plt.plot(0, 0, linewidth=8, color='k', label='HDI {:g}%\n{}'.format(cred_mass*100, hdi_label))
+        hpd_label = hpd_label +  '{:g} {:g}\n'.format(round(value[0], roundto), round(value[1], roundto)) 
+    plt.plot(0, 0, linewidth=8, color='k', label='hpd {:g}%\n{}'.format(cred_mass*100, hpd_label))
 
     plt.xlabel(xlab, fontsize=fontsize)
     plt.ylabel(ylab, fontsize=fontsize)
@@ -78,58 +78,3 @@ def plot_post(param_sample_vec, cred_mass=0.95, comp_val=None,
     frame = plt.gca()
     frame.axes.get_yaxis().set_ticks([])
     return post_summary
-    
-
-def meanplot(trace, vars=None, figsize=None, combined=False, grid=True, 
-              alpha=1, ax=None):
-    """Plot running means
-
-    Parameters
-    ----------
-
-    trace : result of MCMC run
-    vars : list of variable names
-        Variables to be plotted, if None all variable are plotted
-    figsize : figure size tuple
-        If None, size is (12, num of variables * 2) inch
-    combined : bool
-        Flag for combining multiple chains into a single chain. If False
-        (default), chains will be plotted separately.
-    grid : bool
-        Flag for adding gridlines to histogram. Defaults to True.
-    ax : axes
-        Matplotlib axes. Defaults to None.
-
-    Returns
-    -------
-
-    ax : matplotlib axes
-
-    """
-    import matplotlib.pyplot as plt
-    if vars is None:
-        vars = trace.varnames
-
-    n = len(vars)
-
-    if figsize is None:
-        figsize = (6, n*2)
-
-    if ax is None:
-        fig, ax = plt.subplots(n, 1, figsize=figsize, squeeze=False)
-
-    for i, v in enumerate(vars):
-        for d in trace.get_values(v, combine=combined, squeeze=False):
-            rm = np.cumsum(d)/np.arange(1, len(d)+1)
-            ax[i, 0].plot(rm)
-            ax[i, 0].axhline(d.mean(), color="r", lw=1.5, alpha=alpha)
-            ax[i, 0].set_title(str(v))
-            ax[i, 0].grid(grid)
-
-            ax[i, 0].set_ylabel("mean")
-            ax[i, 0].set_xlabel("iteration")
-
-    plt.tight_layout()
-    return ax
-
-
